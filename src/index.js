7cebdaf9dcc805c176fb838903062644f5915b19
Nick <DragooNick@web.de>
2016-07-08 12:50:36 +0200

    MEAN Stack initialized!

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//Hotupdate via Remote
+if (module.hot) {
+  module.hot.accept();
+}
+
+import angular from 'angular';
+import appModule from 'config';
+import 'css/master.scss';
+
+angular.bootstrap(document, [appModule.name]);
