3d15092d149bc00921af98d37dfc1fc2e569102a
Nick <DragooNick@web.de>
2016-07-27 17:38:25 +0200

    sublime scritp added again

---
+++ rsub.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+rsub rsub.sh &
+rsub package.json &
+rsub webpack.config.js &
+rsub src/about/about.html &
+rsub src/index.js &
+rsub src/config/config.js &
+rsub src/config/passport.js &
+rsub src/css/master.scss &
+rsub src/css/todos.scss &
+rsub src/server/routes.js &
+rsub src/server/db/db.js &
+rsub src/server/db/user.js &
+rsub server.js &
+rsub src/todos/todos.html &
+rsub src/todos/todos.js &
+rsub src/factories/todo-factory.js &
+rsub src/server/todos/routes.js &
+rsub src/invMan/invMan.html &
+rsub src/invMan/invMan.js &
+rsub src/factories/invMan-factory.js &
+rsub src/server/invMan/routes.js &
+rsub src/css/invMan.scss &
+rsub src/login/login.html &
+rsub src/login/login.js &
+rsub src/factories/login-factory.js &
+rsub src/server/login/routes.js &
